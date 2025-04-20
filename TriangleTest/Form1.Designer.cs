7310a4ac4d20faaa3b9c0939388052ed6a59084c
Алексей <@ALEKS-PC>
2025-04-20 17:51:36 +0300

    Добавьте файлы проекта.

---
+++ TriangleTest/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TriangleTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(145, 170);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 33);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox2.Location = new Point(334, 170);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 33);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox3.Location = new Point(528, 170);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 33);
+            textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(278, 290);
+            button1.Name = "button1";
+            button1.Size = new Size(222, 50);
+            button1.TabIndex = 3;
+            button1.Text = "Проверить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(227, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(330, 25);
+            label1.TabIndex = 4;
+            label1.Text = "Введите длины сторон треугольника";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(223, 221);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 25);
+            label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            MaximumSize = new Size(816, 489);
+            MinimumSize = new Size(816, 489);
+            Name = "Form1";
+            Text = "Определить треуголник";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ TriangleTest/Form1.cs
@@ -0,0 +1,35 @@
+namespace TriangleTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            uint a, b, c;
+
+            if (UInt32.TryParse(textBox1.Text, out a) &&
+                UInt32.TryParse(textBox2.Text, out b) &&
+                UInt32.TryParse(textBox3.Text, out c))
+            {
+                if (a + b <= c || a + c <= b || b + c <= a)
+                    MessageBox.Show("Это не треугольник");
+                else
+                {
+                    if (a == b && b == c)
+                        MessageBox.Show("Это равносторонний треугольник");
+                    else if (a == b || a == c || b == c)
+                        MessageBox.Show("Это равнобедренный треугольник");
+                    else
+                        MessageBox.Show("Это разносторонний треугольник");
+                }                
+            }
+            else MessageBox.Show("Только целые, положительные числа");
+
+
+        }     
+    }
+}
